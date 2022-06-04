92755c183d8d598949fcf2e1f141662e1e30f224
Mridul Agrahari <mridul2me@gmail.com>
2022-06-04 11:27:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Content from './Content.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Content />
+    </div>
+  );
+}
+
+export default App;
